70afcde918f63f6305aa29999e66cb2cc0c82947
颜鑫宇 <yanxinyu@terminus.io>
2017-05-11 14:13:37 +0800

    版本2

--- src/main/java/com/xg/test/HelloWorld.java
+++ src/main/java/com/xg/test/HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello world");
         int a = 10;
+        int b = 20;
+        System.out.println(a+b);
     }
 }
