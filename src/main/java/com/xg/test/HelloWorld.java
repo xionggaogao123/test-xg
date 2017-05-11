11c38d0ea5ae08fc47605032688889a46d965625
颜鑫宇 <yanxinyu@terminus.io>
2017-05-11 14:08:14 +0800

    Initial commit

---
+++ src/main/java/com/xg/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xg.test;
+
+/**
+ * Created by xionggao on 2017/5/11.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
